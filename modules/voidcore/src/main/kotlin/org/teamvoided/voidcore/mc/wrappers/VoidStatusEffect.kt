67d67f8f206caf90f96d6420ead223f925a8ff57
TheEnderCore <theendercore@gmail.com>
2025-05-24 01:20:25 +0300

    VStatusEffect wrapper for StatusEffect

---
+++ modules/voidcore/src/main/kotlin/org/teamvoided/voidcore/mc/wrappers/VoidStatusEffect.kt
@@ -0,0 +1,12 @@
+@file:Suppress("unused")
+
+package org.teamvoided.voidcore.mc.wrappers
+
+import net.minecraft.entity.effect.StatusEffect
+import net.minecraft.entity.effect.StatusEffectType
+import net.minecraft.particle.ParticleEffect
+
+open class VoidStatusEffect : StatusEffect {
+    constructor(type: StatusEffectType, color: Int) : super(type, color)
+    constructor(type: StatusEffectType, color: Int, particle: ParticleEffect) : super(type, color, particle)
+}
