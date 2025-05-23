af349203b1a6d11508c01aa014ec7965f96400e7
TheEnderCore <theendercore@gmail.com>
2025-05-24 00:13:45 +0300

    Added minecraft serializers

---
+++ modules/voidcore/src/main/kotlin/org/teamvoided/voidcore/serlializers/EntityTypeSerializer.kt
@@ -0,0 +1,22 @@
+package org.teamvoided.voidcore.serlializers
+
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+import net.minecraft.entity.EntityType
+import kotlin.jvm.optionals.getOrNull
+
+
+class EntityTypeSerializer : KSerializer<EntityType<*>> {
+    override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("type", PrimitiveKind.STRING)
+    override fun serialize(encoder: Encoder, value: EntityType<*>) =
+        encoder.encodeString(EntityType.getId(value).toString())
+
+    override fun deserialize(decoder: Decoder): EntityType<*> {
+        val string = decoder.decodeString()
+        return EntityType.get(string).getOrNull() ?: error("Failed to decode EntityType $string")
+    }
+}
