0a8fd033f554b5ee2c5f08c0d4bc61eae9d4bd8c
TheEnderCore <theendercore@gmail.com>
2025-05-24 00:19:37 +0300

    Identifier Helpers

---
+++ modules/voidcore/src/main/kotlin/org/teamvoided/voidcore/helpers/Identifier.kt
@@ -0,0 +1,8 @@
+@file:Suppress("unused")
+
+package org.teamvoided.voidcore.helpers
+
+import net.minecraft.util.Identifier
+
+fun mc(path: String): Identifier = Identifier.ofDefault(path)
+fun createId(namespace: String, path: String): Identifier = Identifier.of(namespace, path)
